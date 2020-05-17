a66fe54cfc6ba8f6213d8c4fccc2c3e1cad64ee9
Roberto Guzman <37600639+rubbertoe-G@users.noreply.github.com>
2020-05-17 18:45:12 -0400

    Update init-mongo.js
    Added comments and separated values for easier setup.

--- init-mongo.js
+++ init-mongo.js
@@ -1,16 +1,27 @@
-db.createUser({
-    user: "testuser",
-    pwd: "testpassword",
-    roles: [{
-        role:"readWrite",
-        db: "IReNEdb"
-    }]
-});
+/**
+ * Database Configuration File
+ * 
+ * Author: Roberto Y. Guzman - roberto.guzman3@upr.edu
+ * 
+ * The service container "irene-db" of the docker-compose.yml file requires this configuration file to 
+ * sets up the database username, user password, user role and database name to be used.
+ * 
+ * The connection string for the database system will be:
+ * 
+ * mongodb://<databaseUser>:<databasePassword>@irene-db:27017/?authSource=admin 
+ * 
+ */
 
-db.collaborators.insertOne({
-    "first_name": "Pepe",
-    "last_name": "Figueroa",
-    "email": "pepe.figueroa@upr.edu",
-    "banned": false,
-    "approved": true
-})
+const databaseUser = "testuser";
+const databasePassword = "testpassword";
+const userRole = "readWrite";
+const databaseToUse = "IReNEdb";
+
+db.createUser({
+  user: databaseUser,
+  pwd: databasePassword,
+  roles: [{
+      role: userRole,
+      db: databaseToUse
+  }]
+});
